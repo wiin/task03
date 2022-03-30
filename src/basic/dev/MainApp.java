b0274d8ea140d26a27419ecbcc873f15e8b27f94
Wiin <win.kinhvanhoa@gmail.com>
2022-03-30 10:06:44 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,31 @@
+package basic.dev;
+/*
+ * GHi chú nhiều dòng
+ * 1. Tên lớp trùng tên file
+ * 2. Tên lớp chữ cái đầu viết hoa. Nếu từ ghép thì những chữ cái đầu viết hoa
+ * 3. Tên lớp đặt phải có ý nghĩa. Ví dụ: class Person : lớp mô tả con người
+ * 		- Interface: phải có yếu tố I. ví dụ: IListener
+ * 		- Abstract: Phải có tiền tố Abstract. Ví dụ: AbstractShape
+ */
+public class MainApp {
+	// Comment 1 dòng cách 1. Phím nóng Ctrl + /
+	/* Comment 1 dòng cách 2 */
+	public static void main (String[] args) {
+		System.out.println("Hello World");
+		/*
+		 * Cách đặt tên biến:
+		 * 1. Không chứa ký tự đặc biệt !@#$%^&*
+		 * 2. Không chứa khoảng trắng
+		 * 3. Viết thường
+		 * 4. Không được bắt đầu bằng số
+		 */
+		int number;
+		String nameSchool;
+	}
+	/* 
+	 * tên phương thức tuân theo quy tắc camelCase
+	 */
+	public void show() {
+		
+	}
+}
